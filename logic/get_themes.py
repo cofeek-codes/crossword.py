4fc1f046dd6c6204f7922bc08a23fad28c6806e4
cofeek-codes <kormyshev11@mail.ru>
2022-05-20 12:39:13 +0500

    init

---
+++ logic/get_themes.py
@@ -0,0 +1,2 @@
+def get_themes():
+    pass
---
+++ main.py
@@ -0,0 +1,12 @@
+from pywebio.output import *
+from pywebio.output import *
+
+from logic.get_themes import get_themes
+
+
+def main():
+    get_themes()
+
+
+if __name__ == '__main__':
+    main()
