e8f7bf2ae13c97fbe0d65da7132294db15122453
cofeek-codes <kormyshev11@mail.ru>
2022-05-21 18:55:07 +0500

    init_themes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from pywebio.output import *
 from pywebio.output import *
 
@@ -13,14 +14,28 @@ def get_words(theme):
 
 
 def main():
+    init_themes()
     themes_storage = open('themes/themes.txt')
     themes = get_themes(themes_storage)
     themes_storage.close()
 
     current_theme_words = get_words(themes[0])
-    print(current_theme_words)
+    # print(current_theme_words)
+
     # get_current theme and prev
     # randomize words
+
+
+def init_themes():
+    themes_storage = open('themes/themes.txt')
+    themes = get_themes(themes_storage)
+    themes_storage.close()
+    for theme in themes:
+        if os.path.exists(f'themes/{theme}') == False:
+            os.mkdir(f'themes/{theme}')
+            theme_words = open(f'themes/{theme}/{theme}.txt', 'w')
+            theme_words.close()
+            print(f'Заполните тему {theme}')
 
 
 if __name__ == '__main__':
