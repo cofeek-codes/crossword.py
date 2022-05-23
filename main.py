7c5f12a198c2ae7ed019b927a45959ffc2a4fac0
cofeek-codes <kormyshev11@mail.ru>
2022-05-23 09:20:54 +0500

    current theme

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ from logic.get_themes import get_themes
 
 
 def get_words(theme):
-    theme_words_file = open(f'themes/{theme}/{theme}.txt')
+    theme_words_file = open(f'themes/{theme}/{theme}.txt', 'r')
     words = theme_words_file.read()
     theme_words_file.close()
     words = words.split('\n')
     return words
 
 
+chosen_theme = input('выбирете тему ')
+
+
 def main():
     themes = init_themes()
-
-    current_theme_words = get_words(themes[0])
-    print(current_theme_words)
-
+    if chosen_theme not in themes:
+        print('Выбраной темы не существует')
+    else:
+        theme_words = get_words(chosen_theme)
+        print(theme_words)
     # get_current theme and prev
     # randomize words
 
