4c66d326a888e46897e782a6bad88d27f53e9477
cofeek-codes <kormyshev11@mail.ru>
2022-05-24 11:58:57 +0500

    table start

--- web/web.py
+++ web/web.py
@@ -4,5 +4,9 @@ from pywebio.input import *
 
 def input_web(randomized_words, chosen_theme):
     put_markdown(f'тема: {chosen_theme}')
+    input_table(randomized_words)
+
+
+def input_table(randomized_words):
     for word in randomized_words:
         put_markdown(word)
