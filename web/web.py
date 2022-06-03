61fc3bf19596088d90376cc946f45c4571a6ec4a
cofeek-codes <kormyshev11@mail.ru>
2022-06-03 10:29:36 +0500

    size

--- web/web.py
+++ web/web.py
@@ -22,4 +22,4 @@ def input_table(randomized_words):
                  put_text(next_letter)],
                 [put_text(next_letter), put_text(
                     next_letter), put_text(next_letter)],
-            ], cell_width='100px', cell_height='100px')
+            ], cell_width='30px', cell_height='30px')
