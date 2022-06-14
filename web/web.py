59d3efd354678249e39006995e97c63dedee2324
cofeek-codes <kormyshev11@mail.ru>
2022-06-14 17:17:47 +0500

    i don't know

--- web/web.py
+++ web/web.py
@@ -15,11 +15,11 @@ def input_table(randomized_words):
         for letter in word:
             if (word.index(letter) + 1) < len(word):
                 next_letter = word[word.index(letter) + 1]
-            else:
-                pass
-            put_grid([
-                [put_text(letter), put_text(next_letter),
-                 put_text(next_letter)],
-                [put_text(next_letter), put_text(
-                    next_letter), put_text(next_letter)],
-            ], cell_width='70px', cell_height='70px').style('font-size:18px; font-weight:bold;')
+
+        put_grid([
+            [put_text(letter), put_text(next_letter),
+                 put_text(letter), put_text(letter), put_text(letter), put_text(letter)],
+            [put_text(next_letter), put_text(
+                next_letter), put_text(letter), put_text(letter), put_text(letter), put_text(letter)],
+        ], cell_width='70px', cell_height='70px').style('font-size:18px; font-weight:bold;')
+        next_letter = int(word.index(next_letter)) + 1
